8a9729483274b9475f995f7b5a6dc4105f968a41
Andrew Albizati <andrew.albizati@gmail.com>
2021-11-28 20:28:44 -0800

    Moved all commands to one file, added README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+# Weather Bot
+This Discord bot is designed to return weather data from OpenWeatherMap for a provided location.
+
+## Setup
+First, go to [the Discord developers page](https://discord.com/developers/docs/intro) and create an application, then register a bot. 
+The bot will need the following permissions:
+
+**OAuth2 scopes:**
+- bot
+- applications.commands
+
+Copy the bot's token to your clipboard.
+
+Next, add a file called `config.json` to the main folder and add the following:
+```json
+{
+    "discord": "discord token goes here",
+    "weather": "OpenWeatherMap api key goes here"
+}
+```
+
+
+
+# Commands
+- **/weather &lt;location>** *Returns the weather for a city/state*
+- **/forecast &lt;location>** *Returns the forecast for a city/state*
+
+## External Dependencies
+- **Pycord** `pip install git+https://github.com/Pycord-Development/pycord`
--- bot.py
+++ bot.py
@@ -1,28 +1,179 @@
 from discord.ext import commands
-
-startup_extensions = ["fcast", "weather"]
+import discord
+from discord import Option
+import asyncio
+from weather_utils import Weather_Utils
+import json
+from datetime import datetime
 
 if __name__ == "__main__":
     # Get tokens from tokens.txt
     # First line = Discord
     # Second line = OpenWeatherMap
-    with open("tokens.txt", "r") as f:
-        lines = f.readlines()
-        lines = [line.rstrip() for line in lines]
+    with open("tokens.json", "r") as f:
+        data = json.load(f)
 
-        TOKEN = lines[0]
+    TOKEN = data["discord"]
+    WEATHER_API_KEY = data["weather"]
     
-    bot = commands.Bot(command_prefix = "!")
+    bot = commands.Bot()
+    weather_utils = Weather_Utils(WEATHER_API_KEY)
 
     @bot.event
     async def on_ready():
         print(f'{bot.user} has connected to Discord!')
 
-    for extension in startup_extensions:
+    @bot.slash_command(description="Returns the weather for a city/state")
+    async def weather(ctx, location: Option(str, "Location", required=True, default=None)):
+        if location == None:
+            return        
+
         try:
-            bot.load_extension(extension)
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Failed to load extension {}\n{}'.format(extension, exc))
+            # Correct if user sent coordinates
+            if len(location.replace(", ", ",").split(",")) == 2:
+                location = location.replace(", ", ",")
     
+                lat = float(location.split(",")[0])
+                lon = float(location.split(",")[1])
+
+                data = weather_utils.getSimpleWeather([lat, lon])      
+            else:
+                data = weather_utils.getSimpleWeather(location)
+        except:
+            await ctx.respond("Location not found!")
+            return
+        
+        # Location not found
+        if data == None:
+            await ctx.respond("Location not found.")
+            return
+        
+        # Find local time for location
+        timestamp = datetime.utcfromtimestamp(data["dt"] + data["timezone"])
+        date = timestamp.strftime("%I:%M:%S %p") # HH:MM:SS AM/PM
+        
+        # Create embed
+        embedVar = discord.Embed(title = "Weather for " + " ".join([word.capitalize() for word in location.split(" ")]) + " (" + date + " local time)", color = 0xad0808)
+        embedVar.description = str(
+        "Temp: **" + str(data["main"]["temp"]) + "** (" + str(data["main"]["temp_min"]) + " - " + str(data["main"]["temp_max"]) + ")\n"
+        "Hum: **" + str(data["main"]["humidity"]) + "%**\n"
+        "*" + str(data["weather"][0]["description"]).capitalize() + "*")
+        
+        embedVar.set_thumbnail(url="https://openweathermap.org/img/wn/" + data["weather"][0]["icon"] + "@2x.png")
+        embedVar.set_footer(text=str(data["coord"]["lat"]) + ", " + str(data["coord"]["lon"]))
+
+        await ctx.respond(embed = embedVar)
+
+
+    @bot.slash_command(description="Returns the forecast for a city/state")
+    async def forecast(ctx, location: Option(str, "Location", required=True, default=None)):
+        if location == None:
+            return
+        
+        initial_message = await ctx.respond("Collecting data...")
+
+        try:
+            # Correct if user sent coordinates
+            if len(location.replace(", ", ",").split(",")) == 2:
+                location = location.replace(", ", ",")
+    
+                lat = float(location.split(",")[0])
+                lon = float(location.split(",")[1])
+
+                data = weather_utils.getForecast([lat, lon])      
+            else:
+                data = weather_utils.getForecast(location)
+        except:
+            await initial_message.delete_original_message()
+            await ctx.channel.send("Location not found!")
+            return
+
+
+        day_descriptions = []
+
+        # Create a list of all days with all info stored in dicts
+        for dayData in data["daily"][:-1]:
+            timestamp = datetime.fromtimestamp(dayData["dt"])
+
+            day_name = timestamp.strftime("%A")
+
+            temp = {}
+            temp["name"] = day_name
+            temp["icon"] = dayData["weather"][0]["icon"]
+
+            temp["value"] = "Temp: **" + str(dayData["temp"]["day"]) + "F** (" + str(dayData["temp"]["min"]) + " - " + str(dayData["temp"]["max"]) +")\nHum: **" + str(dayData["humidity"]) + "%**\n*" + dayData["weather"][0]["description"].capitalize() + "*"
+
+            day_descriptions.append(temp)
+
+        index = 0
+        
+        # Send initial message (first day)
+        embedVar = discord.Embed(title = "Forecast for " + " ".join([word.capitalize() for word in location.split(" ")]), color = 0xad0808)
+
+        embedVar.add_field(name = day_descriptions[index]["name"], value = day_descriptions[index]["value"], inline = False)
+        embedVar.set_thumbnail(url="https://openweathermap.org/img/wn/" + day_descriptions[index]["icon"] + "@2x.png")
+        embedVar.set_footer(text=str(data["lat"]) + ", " + str(data["lon"]))
+
+        await initial_message.delete_original_message()
+        msg = await ctx.channel.send(embed = embedVar)
+
+        await msg.add_reaction("➡️")
+    
+        # Check for reactions added
+        def check(reaction, user):
+            return user == ctx.author and (str(reaction.emoji) == "➡️" or str(reaction.emoji) == "⬅️")
+
+        # Event handler for adding reactions
+        while True:
+            try:
+                reaction, user = await bot.wait_for('reaction_add', timeout=15.0, check=check)
+        
+                await msg.remove_reaction("⬅️", user)
+                await msg.remove_reaction("➡️", user)
+
+                if str(reaction.emoji) == "➡️":
+                    index += 1
+
+                    # Reposition arrows at index 1
+                    if index == 1:
+                        await msg.remove_reaction("➡️", bot.user)
+                        await msg.add_reaction("⬅️")
+                        await msg.add_reaction("➡️")
+
+                    # Remove option to skip forward at end of list
+                    elif index == len(day_descriptions) - 1:
+                        await msg.remove_reaction("➡️", bot.user)
+
+                
+                elif str(reaction.emoji) == "⬅️":
+                    index -= 1
+
+                    # Remove back arrow at index 0
+                    if index == 0:
+                        await msg.remove_reaction("⬅️", bot.user)
+                    
+                    # Add forward arrow at second to last index
+                    elif index == len(day_descriptions) - 2:
+                        await msg.add_reaction("➡️")
+                    
+                
+                # Edit the message with the new day
+                embedVar = discord.Embed(title = "Forecast for " + " ".join([word.capitalize() for word in location.split(" ")]), color = 0xad0808)
+
+                embedVar.add_field(name = day_descriptions[index]["name"], value = day_descriptions[index]["value"], inline = False)
+
+                embedVar.set_thumbnail(url="https://openweathermap.org/img/wn/" + day_descriptions[index]["icon"] + "@2x.png")
+
+                embedVar.set_footer(text=str(data["lat"]) + ", " + str(data["lon"]))
+                await msg.edit(embed = embedVar)
+            except asyncio.TimeoutError:
+                # Timeout
+                try:
+                    await msg.remove_reaction("⬅️", bot.user)
+                    await msg.remove_reaction("➡️", bot.user)
+                except:
+                    pass
+                break
+
+
     bot.run(TOKEN)
